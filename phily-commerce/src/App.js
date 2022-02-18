f10aba7e1c651846793bb745de80fe31186b0ddd
Philip Okanlawon <f.bobo@live.com>
2022-02-18 13:27:25 -0700

    initial homepage

--- phily-commerce/src/App.js
+++ phily-commerce/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './Components/homepage.component';
+
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
