4db658abf538f4be5f0aef3a6c4fc5e300822841
Philip Okanlawon <f.bobo@live.com>
2022-02-27 02:00:36 -0700

    set up shop page with shop data (no css)

--- phily-commerce/src/App.js
+++ phily-commerce/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
