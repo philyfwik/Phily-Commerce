72ef7f354041ace1e8740ce6c85d422bdc827cd8
Philip Okanlawon <f.bobo@live.com>
2022-02-28 13:37:52 -0700

    add a header

--- phily-commerce/src/App.js
+++ phily-commerce/src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './Components/header/header.component';
 import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from './Pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
