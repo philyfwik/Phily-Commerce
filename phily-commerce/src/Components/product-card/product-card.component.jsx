9f20218a14ca2a0162374366113913152928dd40
Philip Okanlawon <f.bobo@live.com>
2022-04-10 17:28:03 -0600

    set up products page and added cart icon/dropdown

---
+++ phily-commerce/src/Components/product-card/product-card.component.jsx
@@ -0,0 +1,20 @@
+import './product-card.styles.css';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton buttonType='inverted'>Add to Cart</CustomButton>
+    </div>
+  );
+}
+
+export default ProductCard;
--- phily-commerce/src/index.js
+++ phily-commerce/src/index.js
@@ -6,12 +6,18 @@ import './index.css';
 import App from './App';
 
 import { UserProvider } from './Contexts/user.context';
+import { ProductsProvider } from './Contexts/products.context';
+import { CartProvider } from './Contexts/cart.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
