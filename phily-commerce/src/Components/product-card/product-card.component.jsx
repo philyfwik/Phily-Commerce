53d5536bca65755e83a745398f7a510af20d8e7b
Philip Okanlawon <f.bobo@live.com>
2022-05-17 16:16:04 -0600

    may 17 most recent

--- phily-commerce/src/Components/product-card/product-card.component.jsx
+++ phily-commerce/src/Components/product-card/product-card.component.jsx
@@ -1,10 +1,17 @@
-import './product-card.styles.css';
+import { useContext } from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
 
+import { CartContext } from '../../Contexts/cart.context';
+
+import './product-card.styles.css';
+
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className='product-card-container'>
       <img src={imageUrl} alt={`${name}`} />
@@ -12,7 +19,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton buttonType='inverted'>Add to Cart</CustomButton>
+      <CustomButton buttonType='inverted' onClick={addProductToCart} >Add to Cart</CustomButton>
     </div>
   );
 }
